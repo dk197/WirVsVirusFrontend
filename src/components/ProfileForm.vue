e4dfc2f3fbb62b9a256da8450e9bb690478d673d
dk197 <38752159+dk197@users.noreply.github.com>
2020-03-22 10:54:32 +0100

    Add form to create placeProfiles

---
+++ src/components/ProfileForm.vue
@@ -0,0 +1,84 @@
+<template>
+    <b-row>
+        <p>Registrieren</p>
+        <b-form @submit.prevent="handleProfileCreate">
+            <b-form-input required v-model="form.vorname" placeholder="Vorname"></b-form-input>
+            <b-form-input required v-model="form.nachname" placeholder="Nachname"></b-form-input>
+            <b-form-input required v-model="form.strasse" placeholder="Straße"></b-form-input>
+            <b-form-input required v-model="form.hausnummer" placeholder="Hausnummer"></b-form-input>
+            <b-form-input required v-model="form.adresszusatz" placeholder="Adresszusatz"></b-form-input>
+            <b-form-input required v-model="form.stadt" placeholder="Stadt"></b-form-input>
+            <b-form-input required v-model="form.plz" placeholder="PLZ"></b-form-input>
+            <b-form-input required v-model="form.land" placeholder="Land"></b-form-input>
+            <b-button type="submit">Erstellen</b-button>
+        </b-form>
+    </b-row>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            form: {
+                vorname: '',
+                nachname: '',
+                strasse: '',
+                hausnummer: '',
+                adresszusatz: '',
+                stadt: '',
+                plz: '',
+                land: ''
+            }
+        }
+    },
+    methods: {
+        async handleProfileCreate() {
+            try {
+                const response = await axios.post(
+                    "http://95.217.162.151:4000/graphql",
+                    {
+                        query: `
+                            mutation Profile($vorname: String!, $nachname: String!, $strasse: String!, $hausnummer: String!, $adresszusatz: String!, $stadt: String!, $plz: String!, $land: String!){
+                                createProfile(vorname: $vorname, nachname: $nachname, strasse: $strasse, hausnummer: $hausnummer, adresszusatz: $adresszusatz, stadt: $stadt, plz: $plz, land: $land) {
+                                    uid
+                                    vorname
+                                    nachname
+                                    strasse
+                                    hausnummer
+                                    adresszusatz
+                                    stadt
+                                    plz
+                                    land
+                                    long
+                                    lat
+                                }
+                            }
+                        `,
+                        variables: {
+                            vorname: this.form.vorname,
+                            nachname: this.form.nachname,
+                            strasse: this.form.strasse,
+                            hausnummer: this.form.hausnummer,
+                            adresszusatz: this.form.adresszusatz,
+                            stadt: this.form.stadt,
+                            plz: this.form.plz,
+                            land: this.form.land
+                        }
+                    },
+                    {headers: { Authorization: 'Bearer ' + localStorage.getItem('token') }},
+                );
+                console.log(response.data);
+                localStorage.setItem('token', response.data.data.login.token)
+                this.$store.commit('logUserIn')
+            }catch(e) {
+                console.log(e);
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,21 +4,25 @@
         <div v-if="!checkIfLoggedIn">
             <LoginForm v-if="showLoginForm"></LoginForm>
             <RegistrationForm v-else></RegistrationForm>
+            <b-button @click="showRegistrationForm">Registrieren</b-button>
         </div>
         <div v-else>
             user is logged in
+            <b-button @click="showProfileForm">Profil anlegen</b-button>
+            <ProfileForm v-if="showProfileFormStatus"></ProfileForm>
         </div>
-        <h1>{{ this.$store.isLoggedIn }}</h1>
     </b-row>
 </template>
 
 <script>
 import LoginForm from '../components/LoginForm.vue'
 import RegistrationForm from '../components/RegistrationForm.vue'
+import ProfileForm from '../components/ProfileForm.vue'
 export default {
     data() {
         return {
-            showLoginForm: true
+            showLoginForm: true,
+            showProfileFormStatus: false
         }
     },
     computed: {
@@ -26,9 +30,18 @@ export default {
             return this.$store.getters.isLoggedIn
         }
     },
+    methods: {
+        showProfileForm() {
+            this.showProfileFormStatus = !this.showProfileFormStatus
+        },
+        showRegistrationForm() {
+            this.showLoginForm = !this.showLoginForm
+        }
+    },
     components: {
         LoginForm,
-        RegistrationForm
+        RegistrationForm,
+        ProfileForm
     }
 }
 </script>
